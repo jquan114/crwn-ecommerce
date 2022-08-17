8f8e456618ba2ae76a8e298e857e4ba6c6b56828
JQUAN CALEK <jquancollins96@gmail.com>
2022-08-16 23:28:37 -0400

    wip:Link to shop page

--- src/routes/home/navigation/navigation.component.jsx
+++ src/routes/home/navigation/navigation.component.jsx
@@ -1,14 +1,23 @@
-import  { Outlet } from 'react-router-dom';
+import { Fragment } from 'react';
+import  { Outlet , Link} from 'react-router-dom';
 
 
 const Navigation = () => {
     return (
-      <div>
-        <div>
-          <h1> I am the navigation bar</h1>
-        </div>
-        <Outlet />
+  <Fragment>
+    <div className='navigation'>
+      <Link className='logo-container' to='/'>
+        <div> Logo</div>
+      </Link>
+      <div className='nav-links-container'>
+        <Link className ='nav-link' to='/shop'>
+          SHOP
+        </Link>
+
       </div>
+    </div>
+    <Outlet />
+  </Fragment>
     );
   };
 
