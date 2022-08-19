74a5d48fc295e3dca234013afcc94bf3d9f4d954
JQUAN CALEK <jquancollins96@gmail.com>
2022-08-19 12:01:01 -0400

    wip:new nav bar with shop link

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import  { Outlet , Link} from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
+import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
