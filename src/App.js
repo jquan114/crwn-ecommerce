56a8fdffcc3af45ef3f00751665cb471a025b0bd
JQUAN CALEK <jquancollins96@gmail.com>
2022-08-21 22:03:37 -0400

    wip:fire base added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route,  } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component.jsx";
+import SignIn from './routes/sign-in/sign-in.component.jsx';
 
 const Shop = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
           <Route path ='/' element={<Navigation/>}>
           <Route index element ={<Home/>} />
           <Route path="/shop" element ={<Shop/>} />
+          <Route path="/sign-in" element ={<SignIn/>} />
           </Route>
       </Routes>
     
