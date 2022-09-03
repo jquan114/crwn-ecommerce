1839782ecbe2b55ef6ea4bc7b70a28ab1fdae117
JQUAN CALEK <jquancollins96@gmail.com>
2022-09-03 10:32:13 -0400

    added new form and button styles

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,11 +2,14 @@ import { connectFirestoreEmulator } from "firebase/firestore";
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
+import Button from '../button/button.component'
 
 import {
   createAuthUserWithEmailAndPassword,
   creatUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
   displayName: "",
@@ -55,8 +58,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1> Sign up with your email and password </h1>
+    <div className ='sign-up-container'>
+        <h2>Don't have an account?</h2>
+      <span> Sign up with your email and password </span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label='Display Name'
@@ -96,7 +100,9 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up </button>
+        <Button  type="submit">
+            Sign Up 
+        </Button>
       </form>
     </div>
   );
