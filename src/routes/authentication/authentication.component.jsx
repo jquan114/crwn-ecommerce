9c26a2a70ab4353331710c15e389ab15a77b24dd
JQUAN CALEK <jquancollins96@gmail.com>
2022-09-04 11:20:21 -0400

    sign in and sign up page almost done

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,20 +4,18 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocfRef = await creatUserDocumentFromAuth(user);
-  };
+const Authentication = () => {
+
 
   return (
     <div>
       <h1> Sign In Page </h1>
-      <button onClick={logGoogleUser}> Sign In With Google</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
