63e831680db8473361cb5747f54feb1e5d4d365c
JQUAN CALEK <jquancollins96@gmail.com>
2022-09-04 18:30:37 -0400

    wip: new sign in and up page for accounts

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -39,7 +39,18 @@ const SignInForm = () => {
       const response = await signInAuthUserWithEmailAndPassword(email,password);
       console.log(response)
       resetFormFields();
-    } catch (error) {}
+    } catch (error) {
+      switch(error.code){
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break
+        case 'auth/user-not-found':
+          alert('no user associated with this email');
+          break
+          default:
+            console.log(error)
+      }
+    }
   };
 
   const handleChange = (event) => {
@@ -72,7 +83,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit"> Sign In </Button>
-          <Button buttonType="google" onClick={signInWithGoogle}>
+          <Button type='button' buttonType="google" onClick={signInWithGoogle}>
             {" "}
             Google Sign In
           </Button>
