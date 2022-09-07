e258463d018f150a3f43b132862e151f8f2553a8
JQUAN CALEK <jquancollins96@gmail.com>
2022-09-07 13:08:39 -0400

    yeayea

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,10 @@
 import { connectFirestoreEmulator } from "firebase/firestore";
-import { useState } from "react";
+import { useState,useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import {UserContext} from '../../contexts/user.context';
 
 import {
   signInWithGooglePopup,
@@ -21,6 +23,8 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
+  const {setCurrentUser} = useContext(UserContext);
+
   console.log(formFields);
 
   const resetFormFields = () => {
@@ -36,8 +40,13 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email,password);
-      console.log(response)
+      const {user} = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+        );
+
+      setCurrentUser(user);
+    
       resetFormFields();
     } catch (error) {
       switch(error.code){
