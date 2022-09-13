2f5be4a7c0517d6e75db3a57e1e18ed2f4adc19e
JQUAN CALEK <jquancollins96@gmail.com>
2022-09-12 21:29:56 -0400

    fetch json data for shop page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import { Routes, Route,  } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component.jsx';
 
-const Shop = () => {
 
-  return (
-
-    <h1> I am the shop page</h1>
-  )
-}
 const App = () => {
   return (
     
