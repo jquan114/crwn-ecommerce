0490c3a261a4d3e730cc778dfa4f86716e706efa
JQUAN CALEK <jquancollins96@gmail.com>
2022-10-13 15:06:41 -0400

    new shop url and fixing bugs

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,16 @@
-import {Routes,Route} from 'react-router-dom';
-import CategoriesPreview from '../../components/categories-preview/categories-preview.component';
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../../components/categories-preview/categories-preview.component";
+
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 
 export const Shop = () => {
-  
   return (
-   <Routes>
-     <Route index element ={<CategoriesPreview/>} />
-   </Routes>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
