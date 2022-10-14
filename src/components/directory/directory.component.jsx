8e90c2c42c3dfda8e69b9e444889006ce1c399e6
JQUAN CALEK <jquancollins96@gmail.com>
2022-10-13 22:30:02 -0400

    new home styles and intergration

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 
 import './directory.styles.scss';
 
@@ -7,10 +7,10 @@ const Directory = ({categories}) => {
     return (
         <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key= {category.id} category={category}/>
+        <DirectoryItem key= {category.id} category={category}/>
         
       ))}
     </div>
-    )
-}
+    );
+};
 export default Directory;
