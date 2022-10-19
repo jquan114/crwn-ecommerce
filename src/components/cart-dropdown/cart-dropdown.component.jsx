1a56298b52eb904342791ff7a811da06e765474c
JQUAN CALEK <jquancollins96@gmail.com>
2022-10-18 21:43:18 -0400

    new styled components for cart icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
-import "./cart-dropdown.styles.scss";
+import {CartDrowndownContainer,EmptyMessage,CartItems} from "./cart-dropdown.styles.jsx";
 
 const CartDowndown = () => {
   const { cartItems } = useContext(CartContext);
@@ -17,15 +17,20 @@ const CartDowndown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDrowndownContainer>
+      <CartItems>
+        {
+          cartItems.length ? (cartItems.map((item) => (
+            <CartItem key={item.id} cartItem={item} />
+          ))) : (
+            <EmptyMessage> Your cart is empty</EmptyMessage>
+          )
+        }
+      
+      </CartItems>
 
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDrowndownContainer>
   );
 };
 
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -2,7 +2,7 @@ import {useContext} from 'react';
 
 import "./product-card.styles.scss";
 
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
 import {CartContext} from '../../contexts/cart.context'
 
@@ -20,7 +20,7 @@ export const ProductCard = ({product}) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted" onClick={addProductToCart} >Add to cart </Button>
+      <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart} >Add to cart </Button>
     </div>
   );
 };
