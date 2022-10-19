efe2b2fb5fd2fdea5b9ffceab1e7e9c5bb8f2130
JQUAN CALEK <jquancollins96@gmail.com>
2022-10-19 18:11:07 -0400

    new title work now working on strip payment system

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component.jsx';
 
+import {HomeTitle} from './home.component.styles.jsx';
 const Home = () => {
 
   return (
+
+  
     <div>
+        <HomeTitle> Welcome To Crown Clothing </HomeTitle>
        <Outlet />
         <Directory />
+        
     </div>
  
   );
